6106d2a0274bc47572a083da4a56783ea963308e
Deeptimayee Maharana <dmaharan@redhat.com>
2023-02-22 23:22:10 +0530

    changes made

--- main.go
+++ main.go
@@ -18,16 +18,16 @@ type todo struct { // struct and slices has been used as database
 	ID        string `json:"id"`
 	Item      string `json:"title"`
 	Duration  string `json:"time"`
-	Completed bool   `json:"completed"`
+	Completed string `json:"completed"`
 }
 
 var todos = []todo{ // slice of type todo has been declared and initialised with some values
-	{ID: "1", Item: "Clean Room", Duration: "10 min", Completed: false},
-	{ID: "2", Item: "Brush Teeth", Duration: "10 min", Completed: false},
-	{ID: "3", Item: "Go For a Walk", Duration: "15 min", Completed: false},
-	{ID: "4", Item: "Breakfast", Duration: "20 min", Completed: false},
-	{ID: "5", Item: "Study", Duration: "1 hour", Completed: false},
-	{ID: "6", Item: "Attend Meeting", Duration: "30 min", Completed: false},
+	{ID: "1", Item: "Clean Room", Duration: "10 min", Completed: "false"},
+	{ID: "2", Item: "Brush Teeth", Duration: "10 min", Completed: "false"},
+	{ID: "3", Item: "Go For a Walk", Duration: "15 min", Completed: "false"},
+	{ID: "4", Item: "Breakfast", Duration: "20 min", Completed: "false"},
+	{ID: "5", Item: "Study", Duration: "1 hour", Completed: "false"},
+	{ID: "6", Item: "Attend Meeting", Duration: "30 min", Completed: "false"},
 }
 
 /*................................ controllers ..................................................*/
